768dd73f14d6598aee2432d7e42fdfcea65814b1
Aaron <J>
2015-08-17 21:48:34 -0700

    Created Pin class.
    Also copied Main.storyboard from the previous incarnation of the
    project.

--- VirtualTourist/Pin.swift
+++ VirtualTourist/Pin.swift
@@ -7,7 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
-class Pin: NSObject {
-   
+class Pin: NSObject
+{
+    // class properties
+    static var droppedPins = [ Int : Pin ]()
+    static var totalPins: Int = 0
+    
+    // instance properties
+    var pinNumber: NSNumber
+    var mapPin: MKPinAnnotationView
+    var coordinate: CLLocationCoordinate2D
+    {
+        return mapPin.annotation.coordinate
+    }
+    // var photoAlbum: [ Photo ] uncomment after implementing Photo class
+    
+    init( mapPin: MKPinAnnotationView )
+    {
+        self.pinNumber = ++Pin.totalPins
+        self.mapPin = mapPin
+        
+        super.init()
+        
+        Pin.droppedPins.updateValue( self, forKey: self.pinNumber as Int )
+    }
 }

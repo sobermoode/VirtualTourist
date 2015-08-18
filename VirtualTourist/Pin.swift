537882135bec2f1cbd87b4e45729c906273a1397
Aaron <J>
2015-08-17 21:29:11 -0700

    Initial commit.
    Starting over. Gonna take what I learned from developing the project
    and try and make it a lot less messy; as well as making a little more
    well-designed.

---
+++ VirtualTourist/Pin.swift
@@ -0,0 +1,13 @@
+//
+//  Pin.swift
+//  VirtualTourist
+//
+//  Created by Aaron Justman on 8/17/15.
+//  Copyright (c) 2015 AaronJ. All rights reserved.
+//
+
+import UIKit
+
+class Pin: NSObject {
+   
+}

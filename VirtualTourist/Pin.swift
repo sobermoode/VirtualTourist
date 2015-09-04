da65688ee0bd115444fe44a2683f5de71736619d
Aaron <J>
2015-09-04 12:44:34 -0700

    Pins are persisting.
    I’m gonna clean up the code and then commit again, just in case I need
    to fall back to a position where I have alternatives I tried but didn’t
    end up using.

--- VirtualTourist/Pin.swift
+++ VirtualTourist/Pin.swift
@@ -8,42 +8,119 @@
 
 import UIKit
 import MapKit
+import CoreData
 
-class Pin: NSObject, MKAnnotation
+@objc( Pin )
+
+class Pin: NSManagedObject, MKAnnotation
 {
     // class properties
-    static var droppedPins = [ Int : Pin ]()
+    // static var droppedPins = [ Int : Pin ]()
     static var currentPinNumber: Int = 0
     
     // instance properties
-    var pinNumber: Int
+    // @NSManaged var pinNumber: Int
+    // @NSManaged var coordinate: CLLocationCoordinate2D
+    @NSManaged var pinLatitude: Double
+    @NSManaged var pinLongitude: Double
+    
     var coordinate: CLLocationCoordinate2D
-    var photoAlbum: [ Photo? ]? = nil
+        {
+        return CLLocationCoordinate2D(latitude: pinLatitude, longitude: pinLongitude)
+    }
+    
+    var photoAlbum: [ Photo? ]? // = nil
     
     // for use with subsequent requests for new photo albums
     var nextFirstImage: Int?
     
-    init( coordinate: CLLocationCoordinate2D )
+    init(
+        coordinate: CLLocationCoordinate2D,
+        context: NSManagedObjectContext
+    )
     {
+        println( "Creating a new Pin..." )
+        let pinEntity = NSEntityDescription.entityForName(
+            "Pin",
+            inManagedObjectContext: context
+        )!
+        
+        super.init(
+            entity: pinEntity,
+            insertIntoManagedObjectContext: context
+        )
+        
         // update the current pin number
         ++Pin.currentPinNumber
+        println( "Current Pin number: \( Pin.currentPinNumber )" )
         
-        self.pinNumber = Pin.currentPinNumber
-        self.coordinate = coordinate
-        
-        super.init()
+        // self.pinNumber = Pin.currentPinNumber
+        // self.willChangeValueForKey("coordinate")
+        // self.setPrimitiveValue(coordinate, forKey: "coordinate")
+        // self.coordinate = coordinate
+        // self.didChangeValueForKey("coordinate")
+        // self.coordinate = coordinate
+        pinLatitude = coordinate.latitude
+        pinLongitude = coordinate.longitude
         
         // the Pin class keeps track of all active Pins
-        Pin.droppedPins.updateValue(
-            self,
-            forKey: Pin.currentPinNumber
-        )
+//        Pin.droppedPins.updateValue(
+//            self,
+//            forKey: Pin.currentPinNumber
+//        )
     }
     
-    class func removePin( pinNumber: Int )
+    override init(
+        entity: NSEntityDescription,
+        insertIntoManagedObjectContext context: NSManagedObjectContext?
+    )
+    {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    class func fetchAllPins() -> [ Pin ]?
+    {
+        println( "fetching all pins..." )
+        let fetchError: NSErrorPointer = nil
+        
+        let pinsFetchRequest = NSFetchRequest( entityName: "Pin" )
+        
+        let pins = CoreDataStackManager.sharedInstance().managedObjectContext!.executeFetchRequest(
+            pinsFetchRequest,
+            error: fetchError
+        )! as! [ Pin ]
+        println( "pins.count: \( pins.count )" )
+        
+        if fetchError != nil
+        {
+            println( "There was an error fetching the pins from Core Data: \( fetchError )." )
+        }
+        
+        Pin.currentPinNumber = pins.count
+        
+        return ( pins.count > 0 ) ? pins : nil
+    }
+    
+    /*
+    required init(coder aDecoder: NSCoder) {
+        let pinEntity = NSEntityDescription.entityForName(
+            "Pin",
+            inManagedObjectContext: CoreDataStackManager.sharedInstance().managedObjectContext!
+            )!
+        super.init(entity: pinEntity, insertIntoManagedObjectContext: CoreDataStackManager.sharedInstance().managedObjectContext!)
+    }
+    */
+    
+    /*
+    func encodeWithCoder(aCoder: NSCoder) {
+        super.encode()
+    }
+    */
+    
+    class func removePin()
     {
         // remove the Pin from the model
-        Pin.droppedPins.removeValueForKey( pinNumber )
+        // Pin.droppedPins.removeValueForKey( pinNumber )
         
         // update the current Pin number
         --Pin.currentPinNumber
